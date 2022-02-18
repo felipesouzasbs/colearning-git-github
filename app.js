74d0892c3651c90495a7d55889ff2695ab5e0f55
Felipe Souza <lfesouza11@gmail.com>
2022-02-18 11:21:18 -0300

    quinto exercicio

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const paises = []
 paises.push('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa'
 )
-console.table(paises)
+//for (const i = 0;i <= paises.lenght; i++){
+//    console.log(paises[i])
+//}
+//paises.forEach((pais) =>{
+//    return console.log(pais)
+//})
+
+for (let pais of paises){
+    console.log(pais)
+}
