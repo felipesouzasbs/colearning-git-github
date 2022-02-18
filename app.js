43ace03a5e9c3ca2d122a21c1ac83e9f7da3bc2a
Felipe Souza <lfesouza11@gmail.com>
2022-02-18 10:59:15 -0300

    Realizado o exercicio

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-const paises = [];
+const paises = []
+paises.push('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa'
+)
+console.table(paises)
